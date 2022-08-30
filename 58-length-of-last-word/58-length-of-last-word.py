989f56119b10098be1831f4fcb6317650e88fa0f
Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>
2022-08-30 19:32:27 +0200

    Time: 6 ms (20.21%), Space: 6.5 MB (51.51%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(s[i] != ' ')
+                ans++;
+            if(s[i] == ' ')
+                if(ans > 0)
+                    return ans;
+        }
+        return ans;
+    }
+};
