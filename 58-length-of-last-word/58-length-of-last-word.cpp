1226c3dc8f96361733bda4a735560f04977ec53a
Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>
2022-08-30 19:38:56 +0200

    Time: 3 ms (58.93%), Space: 6.4 MB (99.34%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,3 +1,13 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split(" ")[-1])
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1, count=0;
+        while(s[i]==' ') i--;
+        while(i!=-1 && s[i]!=' ' )
+        {
+            count++;
+            i--;
+        }
+        return count;
+    }
+};
