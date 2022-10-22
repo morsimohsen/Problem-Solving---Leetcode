7350b11d89328062f815b95be9ae51eda310c4ec
Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>
2022-10-22 15:10:33 +0200

    Time: 0 ms (100.00%), Space: 8.5 MB (37.08%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> ans(nums.size());
-        int sum = 0, c = 0;
-        for (int i = 0; i <nums.size(); i++) {
-            sum += nums[i];
-            ans[i] = sum;
+        vector<int> ans;
+        int sum = 0;
+        for (auto num : nums) {
+            sum += num;
+            ans.push_back(sum);
         }
         
         return ans;
