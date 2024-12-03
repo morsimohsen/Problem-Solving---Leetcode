0aeb17d69a77f106af30d8416bcd2f4809c506f4
Morsi Mohsen Morsi <47028111+morsimohsen@users.noreply.github.com>
2024-12-03 12:09:19 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (8.18%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+       stringstream ss(sentence); // Create a stringstream for splitting the sentence
+        string word;
+        int index = 1;
+
+        while (ss >> word) {
+
+            if (word.substr(0, searchWord.size()) == searchWord) {
+                return index;
+            }
+            index++;
+        }
+
+        return -1;
+        }
+};
