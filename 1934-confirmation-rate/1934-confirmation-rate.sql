76396d66d2e21b510e05527911549a152a96511f
Morsi Mohsen Morsi <47028111+morsimohsen@users.noreply.github.com>
2025-03-26 22:26:23 +0200

    Time: 242 ms (96.43%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    S.user_id, ROUND(AVG(
+        CASE WHEN (C.action='confirmed') THEN 1 ELSE 0 END),2) AS confirmation_rate
+FROM Signups S 
+LEFT JOIN 
+    Confirmations C 
+    ON 
+        S.user_id=C.user_id 
+GROUP BY S.user_id
